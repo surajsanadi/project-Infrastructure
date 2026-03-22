ef1c26c20e63a4ebf799288fe45ac67e2d4c4083
suraj sanadi <surajsanadi008@gmail.com>
2026-03-22 10:25:28 +0000

    updating the container registry

--- Container.tf
+++ Container.tf
@@ -45,7 +45,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
-  admin_enabled       = false
+  admin_enabled       = true
 }
 
 # ── Storage Account + Blob Container ───────────────────────
