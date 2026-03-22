39a59d73886d98c247f639079bc4ec74eb691099
inchara1996 <incharanbhushan1996@gmail.com>
2026-03-22 13:14:25 +0000

    adding all the files required for creating the storage account

---
+++ backendstorage/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_resource_group" "resgroup" {
+  name     = "resgroup"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "stacc" {
+  name                     = "new-storage"
+  resource_group_name      = azurerm_resource_group.resgroup.name
+  location                 = azurerm_resource_group.resgroup.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                 = "container123"
+  storage_account_name = azurerm_storage_account.stacc.name
+}
